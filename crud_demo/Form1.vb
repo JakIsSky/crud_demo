285d70d2093d5635321bd91f4204e13b64918b47
JakIsSky <donedaabner@gmail.com>
2025-11-21 08:20:04 +0800

    read button is working

--- crud_demo/Form1.vb
+++ crud_demo/Form1.vb
@@ -1,10 +1,14 @@
+﻿
+Imports MySql.Data.MySqlClient
+
 ﻿Imports MySql.Data.MySqlClient
+
 Public Class Form1
     Dim conn As MySqlConnection
     Dim COMMAND As MySqlCommand
+
     Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
         conn = New MySqlConnection
-
         conn.ConnectionString = "server=localhost; userid=root; password=root; database=crud_demo_db;"
 
         Try
@@ -14,12 +18,10 @@ Public Class Form1
             MessageBox.Show(ex.Message)
             conn.Close()
         End Try
-
     End Sub
 
     Private Sub ButtonInsert_Click(sender As Object, e As EventArgs) Handles ButtonInsert.Click
-        Dim query As String = "INSERT INTO student_tbl (name, age, email) VALUES (@name, @age, @email)"
-
+        Dim query As String = "INSERT INTO students_tbl (name, age, email) VALUES (@name, @age, @email)"
         Try
             Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
                 conn.Open()
@@ -30,6 +32,20 @@ Public Class Form1
                     cmd.ExecuteNonQuery()
                     MessageBox.Show("Record insert successfully!")
                 End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonRead_Click(sender As Object, e As EventArgs) Handles ButtonRead.Click
+        Dim query As String = "SELECT * FROM crud_demo_db.students_tbl;"
+        Try
+            Using conn As New MySqlConnection("server=localhost; userid=root; password=root; database=crud_demo_db;")
+                Dim adapter As New MySqlDataAdapter(query, conn) ' Get from Database
+                Dim table As New DataTable() ' Table Object
+                adapter.Fill(table) ' From Adapter to Table Object
+                DataGridView1.DataSource = table ' Display to DataGridView
             End Using
         Catch ex As Exception
             MsgBox(ex.Message)
